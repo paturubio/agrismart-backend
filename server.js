fc63a7703fab26f631dba108e8935e50b4c192ad
Patu <99687749+paturubio@users.noreply.github.com>
2026-03-15 15:18:37 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -8,13 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
- res.send("Backend Agrismart funcionando ");
+app.get("/", (req, res) => {
+  res.send("Agrismart API running");
 });
+
 app.use("/api/auth", authRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
